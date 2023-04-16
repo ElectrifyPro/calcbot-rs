e0f414d1d33b35cc025dd8c053924c68a18abf29
Tam Pham <tam001p@gmail.com>
2023-04-15 23:16:16 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+use dotenv::dotenv;
+use std::{env, error::Error, sync::Arc};
+use twilight_cache_inmemory::{InMemoryCache, ResourceType};
+use twilight_gateway::{Event, Intents, Shard, ShardId};
+use twilight_http::Client as HttpClient;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    dotenv()?;
+    let token = env::var("DISCORD_TOKEN")?;
+
+    // Specify intents requesting events about things like new and updated
+    // messages in a guild and direct messages.
+    let intents = Intents::GUILDS | Intents::GUILD_MESSAGES | Intents::DIRECT_MESSAGES | Intents::MESSAGE_CONTENT;
+
+    // Create a single shard.
+    let mut shard = Shard::new(ShardId::ONE, token.clone(), intents);
+
+    // The http client is separate from the gateway, so startup a new
+    // one, also use Arc such that it can be cloned to other threads.
+    let http = Arc::new(HttpClient::new(token));
+
+    // Since we only care about messages, make the cache only process messages.
+    let cache = InMemoryCache::builder()
+        .resource_types(ResourceType::MESSAGE)
+        .build();
+
+    // Startup the event loop to process each event in the event stream as they
+    // come in.
+    loop {
+        let event = match shard.next_event().await {
+            Ok(event) => event,
+            Err(source) => {
+                if source.is_fatal() {
+                    break;
+                }
+
+                continue;
+            }
+        };
+        // Update the cache.
+        cache.update(&event);
+
+        // Spawn a new task to handle the event
+        tokio::spawn(handle_event(event, Arc::clone(&http)));
+    }
+
+    Ok(())
+}
+
+async fn handle_event(
+    event: Event,
+    http: Arc<HttpClient>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match event {
+        Event::MessageCreate(msg) if msg.content == "!ping" => {
+            http.create_message(msg.channel_id).content("Pong!")?.await?;
+        }
+        Event::Ready(_) => {
+            println!("Shard is ready");
+        }
+        _ => {}
+    }
+
+    Ok(())
+}
